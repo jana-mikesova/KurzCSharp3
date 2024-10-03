ac8833cab1087829ccadb20c64cb6a1518e4ce67
jana-mikesova <mikesovaj@gmail.com>
2024-10-03 12:38:13 +0200

    Greed - extra - num of greeds

--- sandbox/katas/Greed.01/Greed/Greed.cs
+++ sandbox/katas/Greed.01/Greed/Greed.cs
@@ -107,4 +107,175 @@ public class Greed
         Random random = new Random();
         return random.Next(1, 7);
     }
+
+    public int GetNumberOfGreeds()
+    {
+        int numOfGreeds;
+        Console.WriteLine("Zadej, kolik kostek bude hozeno (1-6):");
+        while(true)
+        {
+            string userInput = Console.ReadLine();
+            bool parseResult = int.TryParse(userInput, out numOfGreeds);
+            if (parseResult)
+            {
+                if(numOfGreeds < 1 || numOfGreeds > 6)
+                {
+                    Console.WriteLine("Zadané číslo je mimo povolený rozsah 1-6, zadej znovu.");
+                } else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Chybné zadání, zadejte znovu celé číslo v rozmezí 1-6.");
+            }
+        }
+        return numOfGreeds;
+    }
+
+    public int CountScoreExtra(int[] greeds)
+    {
+        int scoreExtra = 0;
+        int[] numbers = CountNumbers(greeds);
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == 6)
+            {
+                switch (i)
+                {
+                    case 0:
+                        scoreExtra += 8 * 1000;
+                        break;
+                    case 1:
+                        scoreExtra += 8 * 200;
+                        break;
+                    case 2:
+                        scoreExtra += 8 * 300;
+                        break;
+                    case 3:
+                        scoreExtra += 8 * 400;
+                        break;
+                    case 4:
+                        scoreExtra += 8 * 500;
+                        break;
+                    case 5:
+                        scoreExtra += 8 * 600;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (numbers[i] == 5)
+            {
+                switch (i)
+                {
+                    case 0:
+                        scoreExtra += 4 * 1000;
+                        break;
+                    case 1:
+                        scoreExtra += 4 * 200;
+                        break;
+                    case 2:
+                        scoreExtra += 4 * 300;
+                        break;
+                    case 3:
+                        scoreExtra += 4 * 400;
+                        break;
+                    case 4:
+                        scoreExtra += 4 * 500;
+                        break;
+                    case 5:
+                        scoreExtra += 4 * 600;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (numbers[i] == 4)
+            {
+                switch (i)
+                {
+                    case 0:
+                        scoreExtra += 2 * 1000;
+                        break;
+                    case 1:
+                        scoreExtra += 2 * 200;
+                        break;
+                    case 2:
+                        scoreExtra += 2 * 300;
+                        break;
+                    case 3:
+                        scoreExtra += 2 * 400;
+                        break;
+                    case 4:
+                        scoreExtra += 2 * 500;
+                        break;
+                    case 5:
+                        scoreExtra += 2 * 600;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (numbers[i] == 3)
+            {
+                switch (i)
+                {
+                    case 0:
+                        scoreExtra += 1000;
+                        break;
+                    case 1:
+                        scoreExtra += 200;
+                        break;
+                    case 2:
+                        scoreExtra += 300;
+                        break;
+                    case 3:
+                        scoreExtra += 400;
+                        break;
+                    case 4:
+                        scoreExtra += 500;
+                        break;
+                    case 5:
+                        scoreExtra += 600;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (numbers[i] is 1)
+            {
+                switch (i)
+                {
+                    case 0:
+                        scoreExtra += 100;
+                        break;
+                    case 4:
+                        scoreExtra += 50;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (numbers[i] is 2)
+            {
+                switch (i)
+                {
+                    case 0:
+                        scoreExtra += 200;
+                        break;
+                    case 4:
+                        scoreExtra += 100;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        return scoreExtra;
+    }
 }
--- sandbox/katas/Greed.01/Greed/Program.cs
+++ sandbox/katas/Greed.01/Greed/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// zakladni verze skorovani, hazi se vzdy peti kostkami
+Console.WriteLine("Základní verze");
+
 Greed greed = new Greed();
 
 int numberOfGreeds = 5;
@@ -14,5 +17,25 @@ for (int i = 0; i < thrownValues.Length; i++)
 Console.WriteLine(thrownValuesString);
 
 int score = greed.CountScore(thrownValues);
-Console.WriteLine(score);
+Console.WriteLine("Skóre: " + score);
 
+
+// modifikovana verze skorovani, hazi se 1 - 6 kostkami
+Console.WriteLine();
+Console.WriteLine("Extra verze");
+
+Greed greedExtra = new Greed();
+
+int numberOfGreedsExtra = greedExtra.GetNumberOfGreeds();
+int[] thrownValuesExtra = new int[numberOfGreedsExtra];
+string thrownValuesExtraString = "Hozeno: ";
+
+for (int i = 0; i < thrownValuesExtra.Length; i++)
+{
+    thrownValuesExtra[i] = greed.ThrowGreed();
+    thrownValuesExtraString += thrownValuesExtra[i] + " ";
+}
+Console.WriteLine(thrownValuesExtraString);
+
+int scoreExtra = greedExtra.CountScoreExtra(thrownValuesExtra);
+Console.WriteLine("Skóre: " + scoreExtra);
